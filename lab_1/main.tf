8f01e2c230435ae8c3379da64e38af088ad53e4f
Prakash Rana <prana12@live.com>
2021-05-01 19:30:47 +0100

    Lab 1 changes

---
+++ lab_1/main.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "website" {
+  bucket = "playground-${var.my_panda}.devopsplayground.org"
+  acl    = "public-read"
+
+  tags = {
+    ManagedBy = "terraform"
+  }
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "index.html"
+  source       = "./content/index.html"
+  acl          = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_object" "image" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "image.jpeg"
+  source      = "./content/image.jpeg"
+  acl          = "public-read"
+  content_type = "image/jpeg"
+}
+
+data "aws_route53_zone" "main" {
+  name         = "devopsplayground.org"
+  private_zone = false
+}
+
+resource "aws_route53_record" "link" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "playground-${var.my_panda}"
+  type    = "CNAME"
+  ttl     = "300"
+
+  records = [aws_s3_bucket.website.website_endpoint]
+}
---
+++ lab_1/variables.tf
@@ -0,0 +1,4 @@
+variable "my_panda" {
+  default = "feasible-panda"
+  description = "The name of your panda (provided with environment) i.e. happy-panda"
+}
